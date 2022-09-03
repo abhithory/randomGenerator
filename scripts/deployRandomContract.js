d2748cee9b973e5910c68e4c96bf1a4f9a2b67fe
theteamisbest <theteamisbest@example.com>
2022-09-03 19:15:24 +0530

    deploy script changed

--- scripts/deployRandomContract.js
+++ scripts/deployRandomContract.js
@@ -7,8 +7,8 @@ async function main(){
     console.log("1");
     const [owner,user,_] = await hre.ethers.getSigners();
     console.log("owner: ",owner.address);
-    console.log("user: ",user.address);
-    const randomGeneratorContract = await _randomGeneratorContract.deploy(owner.address,user.address);
+    console.log("user: ",user);
+    const randomGeneratorContract = await _randomGeneratorContract.deploy(owner.address,owner.address);
     console.log("wait... deploying");
     await randomGeneratorContract.deployed();
 
